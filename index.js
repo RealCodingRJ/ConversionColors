3f6a991d6b1ee520a6bd928da9627d08f92b2b10
Ryan James <realcoder2023@gmail.com>
2025-05-23 23:45:31 -0400

    addded a class to code to make it more useable

--- index.js
+++ index.js
@@ -2,17 +2,36 @@ let r = Math.floor(Math.random() * 255);
 let g = Math.floor(Math.random() * 255);
 let b = Math.floor(Math.random() * 255);
 
-let Colors ={
-    red: r,
-    green: g,
-    blue: b
+class Conversion {
+    r = 0;
+    g = 0;
+    b = 0;
+
+    constructor(r, g, b) {
+
+        this.r = r;
+        this.g = g;
+        this.b = b;
+
+    }
+
+    ToRGB() {
+        console.log("R: " + r)
+        console.log("G: " + g)
+        console.log("B: " + b)
+
+    }
+
+    ToHex() {
+
+
+        console.log( r.toString(16) +
+            g.toString(16) + b.toString(16));
+
+    }
 }
 
+const Color = new Conversion(r, g, b);
 
-console.log("R: " + Colors.red)
-console.log("G: " + Colors.green)
-console.log("B: " + Colors.blue)
-
-
-console.log( Colors.red.toString(16)
-    + Colors.green.toString(16) + Colors.blue.toString(16));
+Color.ToRGB();
+Color.ToHex();
